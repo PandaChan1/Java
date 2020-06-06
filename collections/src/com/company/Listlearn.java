ed9cb929fe182295d24ba147dfc229be52bdaa6a
PandaChan1 <chenyang.74463@gmail.com>
2020-06-06 12:02:48 +0800

    collections

---
+++ collections/src/com/company/Listlearn.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Listlearn {
+
+    public static void main(String[] args) {
+	// write your code here
+        List<Person> list=List.of(new Person("xiao","ming",18),
+                new Person("xiao","li",22),
+                new Person("bob","smith",99));
+        boolean exist=list.contains(new Person("bob","smith",99));
+        System.out.println(exist ?"测试成功":"测试失败");
+    }
+
+}
+class Person{
+    String firstName;
+    String LastName;
+    int age;
+    public Person(String firstName,String LastName,int age){
+        this.firstName=firstName;
+        this.LastName=LastName;
+        this.age=age;
+    }
+    public boolean equals(Object o){
+        if(o instanceof Person){
+            Person p= (Person) o;
+            return Objects.equals(this.firstName,p.firstName)&&Objects.equals(this.LastName,p.LastName)&& this.age==p.age ;
+        }
+
+        return false;
+    }
+}
