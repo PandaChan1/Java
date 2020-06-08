d04f6898b6de695148ae96f398b8476f250b8c43
PandaChan1 <chenyang.74463@gmail.com>
2020-06-08 16:59:02 +0800

    collections

---
+++ collections/src/com/company/allc.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class allc {
+    public static void main(String [] args){
+        int start,end;
+        Scanner sc=new Scanner(System.in);
+            start=sc.nextInt();
+            end=sc.nextInt();
+            sc.close();
+        System.out.println( solution(start,end));
+    }
+  private static int solution(int start, int end){
+        int ans=0;
+      Queue<Integer> queue=new LinkedList<>();
+      for (int i=start;i<=end;i++){
+          queue.offer(i);
+      }
+      int one=queue.poll();
+      while (queue.size()!=0){
+          if (one%3==0){
+              ans++;
+          }
+          one=one*10+queue.poll();
+      }
+
+      return ans;
+
+    }
+}
