8e97c0614d426fc7efabb14117991f2a8ebb7e73
PandaChan1 <chenyang.74463@gmail.com>
2020-09-17 18:24:44 +0800

    快速排序

--- TopTenSortingAlg/src/SortAlg/QuickSort.java
+++ TopTenSortingAlg/src/SortAlg/QuickSort.java
@@ -1,62 +1,39 @@
 package SortAlg;
 
 public class QuickSort {
-    public static int[] quickSort(int[] array,int start,int end){
-        if (array.length<1 || start<0 || end>=array.length || start>end) return null;
-        int smallIndex=partition(array,start,end);
-        if (smallIndex>start){
-            quickSort(array,start,smallIndex-1);
-        }
-        if (smallIndex<end){
-            quickSort(array,smallIndex+1,end);
-        }
-        return array;
-    }
-
-    private static int partition(int[] array, int start, int end) {
-        int pivot=(int)(start+Math.random()*(end-start+1));
-        int smallIndex=start-1;
-        swap(array,pivot,end);
-        for (int i=start;i<=end;i++){
-            if (array[i]<=array[end]){
-                smallIndex++;
-                if (i>smallIndex){
-                    swap(array,i,smallIndex);
-                }
-            }
-
-        }
-        return smallIndex;
-    }
-
-    private static void swap(int[] array, int i, int j) {
-        int temp=array[i];
-        array[i]=array[j];
-        array[j]=temp;
-    }
-
-
     public static int[] quick(int[] array,int left,int right){
         int pivot=0;
         if (left<right){
+            //找基准
             pivot=part(array,left,right);
+            //递归
             quick(array,left,pivot-1);
             quick(array,pivot+1,right);
         }
         return array;
     }
     public static int part(int[] array,int left,int right){
+        //设置基准为左边的元素
         int key=array[left];
+        /**
+         * 保证基准左边的数都大于基准，右边的数都小于基准
+         */
         while(left<right){
             while(left<right && array[right]>=key){
+                //右边元素大于基准，right向左移
                 right--;
             }
+            //否则，将小于基准的元素放到左边
             array[left]=array[right];
+
             while(left<right&&array[left]<=key){
+                //左边的元素小于基准，left向右移
                 left++;
             }
+            //否则，将大于基准的元素放到右边
             array[right]=array[left];
         }
+        //最后将key赋给left，此时left左边都小于array[left]，右边都大于array[left]
         array[left]=key;
         return left;
     }
