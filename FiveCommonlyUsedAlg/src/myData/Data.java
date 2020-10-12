740718b24b6cbda2a6940d38b938acd44d6f242b
PandaChan1 <chenyang.74463@gmail.com>
2020-10-12 18:57:59 +0800

    commit

--- FiveCommonlyUsedAlg/src/myData/Data.java
+++
@@ -1,7 +0,0 @@
-package myData;
-
-public class Data {
-    public static final int[] start={1,2,0,5,3,5,6,8,8,2,12};
-    public static final int[] end={4,5,6,7,8,9,10,11,12,13,14};
-    public static final int[] result={1,4,8,11};
-}
