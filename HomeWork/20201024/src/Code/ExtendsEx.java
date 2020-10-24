23bb0d5c5b7f7220bf0ae45ccb4e0a6383b836bf
PandaChan1 <chenyang.74463@gmail.com>
2020-10-24 20:54:55 +0800

    20201024--PM

---
+++ HomeWork/20201024/src/Code/ExtendsEx.java
@@ -0,0 +1,19 @@
+package Code;
+/**
+ *@ClassName: ExtendsEx
+ *@Description 继承例子
+ *@Author PandaChan1
+ *@Date 2020/10/24
+ *@Time 18:43
+ */
+
+class Animal{
+    public String name;
+    public void eat(){
+        System.out.println("吃东西");
+    }
+}
+
+public class ExtendsEx {
+
+}
