6b59abb667fd2de6f833ee7c2e4e4e78e75a915f
PandaChan1 <chenyang.74463@gmail.com>
2020-10-26 12:31:48 +0800

    Cloneable接口

--- HomeWork/20201026/src/Code/CloneableTest.java
+++ HomeWork/20201026/src/Code/CloneableTest.java
@@ -7,6 +7,26 @@ package Code;
  *@Time 11:29
  */
 
+class Person implements Cloneable{
+    protected String name;
+    protected  int age;
 
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
 public class CloneableTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Person person1 = new Person();
+        person1.name = "chen";
+        person1.age = 22;
+        Person person2 = (Person) person1.clone();
+        System.out.println(person1.name);
+        System.out.println(person2.name);
+        System.out.println("==============================");
+        person2.name = "wang";
+        System.out.println(person1.name);
+        System.out.println(person2.name);
+    }
 }
