5df224b71749bc5b2e6438d7b2579a3ae641c3d8
PandaChan1 <chenyang.74463@gmail.com>
2020-11-16 18:40:51 +0800

    二叉搜索树删除

--- HomeWork/Generic/src/BSTree/BinarySearchTree.java
+++ HomeWork/Generic/src/BSTree/BinarySearchTree.java
@@ -60,8 +60,95 @@ class BSTree{
     }
 
     //删除
-    public void remove(int key) {
+    public boolean remove(int key) {
+        Node current = root;
+        Node pre = root;
+        boolean isLeft = true;  //标记是否为左节点
 
+        //查询是否存在该节点
+        while(current.data != key) {
+            pre = current;
+            if (current.data > key) {
+                isLeft = true;
+                current = current.left;
+            }else {
+                isLeft = false;
+                current = current.right;
+            }
+            if (current == null) {
+                return false;
+            }
+        }
+        //此时，current.data == key
+        //第一种情况  current左右节点都为空
+
+        if (current.left == null && current.right == null) {
+            if (current == root) {  //判断是不是根
+                root = null;
+            }else {
+                if (isLeft) {  //判断是不是左节点
+                    pre.left = null;
+                }else {
+                    pre.right = null;
+                }
+            }
+            return true;
+        }else if (current.left  == null || current.right == null){   //左右节点有一个为空
+            if (current.left == null) {  //左节点为空
+                if (current == root) {  //判断根节点
+                    root = root.right;
+                }else {
+                    if (isLeft) {  //current是左节点
+                        pre.left = current.right;
+                    }else {
+                        pre.right = current.right;
+                    }
+                }
+                return true;
+            }else {  //右节点为空
+                if (current == root) {
+                    root = root.left;
+                }else {
+                    if (isLeft) {
+                        pre.left = current.left;
+                    }else {
+                        pre.right = current.left;
+                    }
+                }
+                return true;
+            }
+        }else {  //删除的节点左右都不为空
+        Node newNode = getNewNode(current);
+            if (newNode == root) {
+                root = newNode;
+            }else {
+                if (isLeft) {
+                    pre.left = newNode;
+                }else {
+                    pre.right = newNode;
+                }
+            }
+            newNode.left = current.left;
+            return true;
+        }
+
+    }
+
+    private Node getNewNode(Node del) {
+        Node parent = del;
+        Node find = del;
+        Node cur = del.right;
+        while(cur != null) {
+            parent = find;
+            find = cur;
+            cur = cur.left;
+        }
+        //此时cur为空
+        if (del.right != find) {  //如果要删除的节点右子树有左子
+            parent.left = find.right;
+            find.right = del.right;
+        }
+        return find;
     }
 
     public void pre(Node root) {
@@ -88,6 +175,9 @@ public class BinarySearchTree {
         bsTree.put(15);
         bsTree.put(2);
         bsTree.put(45);
+        bsTree.put(17);
+        bsTree.put(16);
+        bsTree.put(25);
 
         bsTree.pre(bsTree.root);
         System.out.println();
@@ -100,6 +190,10 @@ public class BinarySearchTree {
             e.printStackTrace();
             System.out.println("树中没有该数据");
         }
+        bsTree.remove(15);
+        bsTree.pre(bsTree.root);
+        System.out.println();
+        bsTree.in(bsTree.root);
 
     }
 
