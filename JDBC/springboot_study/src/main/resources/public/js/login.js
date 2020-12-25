2dec931895de125b1e64f0858383600ce57d88cf
PandaChan1 <chenyang.74463@gmail.com>
2020-12-25 12:23:14 +0800

    springboot使用

--- JDBC/springboot_study/src/main/resources/public/js/login.js
+++ JDBC/springboot_study/src/main/resources/public/js/login.js
@@ -2,13 +2,24 @@ $(function main() { //html加载完执行方法
     $("#login_form").submit(function () {  //绑定表单的提交事件
 
         $.ajax({
-            url:"data/login.json",
+           url:"../data/login.json",
+           //  url:"../data/login2.json",
             type:"get",  // 请求方法
             //dataType:"x-www-form-urlencoded"  //请求数据类型
             data:$("#login_form").serialize(),  //使用表单数据
             contentType:"application/json", //响应数据类型
             success:function (resp) {
-            alert(JSON.stringify(resp))   //提示框显示JSON对象转为字服符串
+                //success为true跳转到main页面，否则提示code和message
+                if (resp.success) {
+                    //把地址栏url改为指定的值
+                    window.location.href = "../views/main.html";
+                }else{
+                    alert("错误码：" + resp.code + "\n" + "错误信息：" + resp.message)
+                }
+                //alert(JSON.stringify(resp))   //提示框显示JSON对象转为字符串
+            },
+            error:function (req,testStatus,err) {
+                alert(req.status)
             }
         })
         //alert("submit");
