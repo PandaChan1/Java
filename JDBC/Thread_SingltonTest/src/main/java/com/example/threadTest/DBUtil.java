d5c817604bfa0ec091f4bc8676ed9d5c56a1b7a5
PandaChan1 <chenyang.74463@gmail.com>
2021-01-03 18:45:19 +0800

    线程+锁

---
+++ JDBC/Thread_SingltonTest/src/main/java/com/example/threadTest/DBUtil.java
@@ -0,0 +1,21 @@
+package com.example.threadTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBUtil {
+    //单例模式
+    private static  DataSource dataSource;
+    public static Connection getConnection() throws SQLException {
+        if (dataSource == null) {
+                synchronized (DBUtil.class) {
+                    if (dataSource == null) {
+                        //初始化
+                    }
+                }
+        }
+
+        return dataSource.getConnection();
+    }
+}
