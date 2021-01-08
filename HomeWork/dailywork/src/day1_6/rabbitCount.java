15ce6471d38bb2f8242c21df9a0def9b25cf17e2
PandaChan1 <chenyang.74463@gmail.com>
2021-01-08 14:09:14 +0800

    牛客oj兔子总数（斐波那契）+密码解密

--- HomeWork/dailywork/src/day1_6/rabbitCount.java
+++ HomeWork/dailywork/src/day1_6/rabbitCount.java
@@ -1,6 +1,14 @@
 package day1_6;
 
 import java.util.Scanner;
+/**
+ *@ClassName: rabbitCount
+ *@Description 有一只兔子，第三天开始每天生一只小兔子。且生出的兔子每三天也会生一只兔子，求第n天总共有多少只兔子
+ *@Author PandaChan1
+ *@Date 2021/1/8
+ *@Time 14:08
+ */
+
 
 public class rabbitCount {
     public static void main(String[] args) {
--- HomeWork/dailywork/src/day1_6/secrect.java
+++ HomeWork/dailywork/src/day1_6/secrect.java
@@ -2,6 +2,15 @@ package day1_6;
 
 import java.util.Scanner;
 
+/**
+ *@ClassName: secrect
+ *@Description 解密  输入经过加密的密码，加密规则为 ： 将该字母变为其后面的第五个字母 ，先给出加密后的密码，求原密码
+ *@Author PandaChan1
+ *@Date 2021/1/8
+ *@Time 14:06
+ */
+
+
 public class secrect {
    static char chars[] = new char[]{
             'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
